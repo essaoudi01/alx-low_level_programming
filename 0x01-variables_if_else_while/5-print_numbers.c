ec4e63f6bf119fe77feccdf3e434d6c6ca9687c9
essaoudi01 <aymaneessaoudi68@gmail.com>
2023-03-20 19:12:18 -0700

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,4 +1,4 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
  * main - Entry point
@@ -17,7 +17,7 @@ int main(void)
 		printf("%i", digit);
 		digit++;
 	}
-	printf("\n")
+	printf("\n");
 		return (0);
 }
 
