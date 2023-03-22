9445ab19c4ed1bbf111bc4aad21f869e44f2356d
essaoudi01 <aymaneessaoudi68@gmail.com>
2023-03-21 22:02:47 -0700

    a

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - Entry point
+ *
+ * Description:c program that print alphabet lowercase
+ *
+ */
+
+void print_alphabet(void);
+{
+	char *al = "abcdefghijklmnopqrstuvwxyz";
+		for(int i = 0; i < 26;i++)
+		{
+			putchar(al[i]);
+		}
+	putchar('\n');
+
+
+}
