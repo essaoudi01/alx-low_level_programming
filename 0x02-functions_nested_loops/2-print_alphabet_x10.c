cf3a47a4a74bf533ba5e387f7273b73c1b40221e
essaoudi01 <aymaneessaoudi68@gmail.com>
2023-03-21 23:05:50 -0700

    a

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,13 @@
  */
 void print_alphabet_x10(void)
 {
-	int i = 0;
-	while (i<10)
-{
-
-	int ch = 'a';
+	int line, ch;
+	for (line = 0; line <=9; line++)
 	{
-	for (int ch = 'a'; ch <= 'z'; ch++)
-		putchar(ch);
-	}
-        putchar('\n');
-	i++;
-}
+		for (ch = 'a'; ch <= 'z'; ch++)
+			putchar(ch);
+		putchar('\n')
+
 
 
 
